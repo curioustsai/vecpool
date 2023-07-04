2ec6cad01ce5de6af7a94a6108a6daedd68e880d
richardtsai <richardtsai@fortemedia.com>
2023-07-04 13:50:54 +0800

    (update) FMSmartVecToDat.ps1

--- Utils/FMSmartVecToDat.ps1
+++ Utils/FMSmartVecToDat.ps1
@@ -1,14 +1,32 @@
 param(
-     [Parameter()]
-     [string]$datname
- )
- 
+    [Parameter(Mandatory)]
+    [ValidateSet('d2v', 'v2d')]
+    [string]$mode,
 
- $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
- $artifacts=$datname -replace ".dat", "_d2v.vec"
- $vecs=$datname -replace ".dat", ".vec"
+    [Parameter(Mandatory)]
+    [string]$filename
+    )
 
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 
-Get-ChildItem $datname | ForEach-Object {FMSmartVecToDat.exe -d2v $_.fullname}
-Get-ChildItem $artifacts | Rename-Item -NewName {$_.fullname -replace "_d2v", ""}
-Get-ChildItem $vecs | ForEach-Object { python $scriptPath/pyvec/format.py -i $_ -o $_ }
+if ($mode -eq 'd2v') {
+  $artifacts=$filename -replace ".dat", "_d2v.vec"
+
+  Get-ChildItem $filename | ForEach-Object {FMSmartVecToDat.exe -d2v $_.fullname}
+  Get-ChildItem $artifacts | ForEach-Object { python $scriptPath/pyvec/format.py -i $_ -o $_ }
+  Get-ChildItem $artifacts | ForEach-Object {
+    $newname = $_.fullname -replace '_d2v.vec', '.vec'
+    if (Test-Path -Path $newname) { Remove-Item -Path $newname }
+    Rename-Item -Path $_.FullName -NewName $newname
+    }
+
+} elseif ($mode -eq 'v2d') {
+  $artifacts=$filename -replace ".vec", "_v2d.dat"
+
+  Get-ChildItem $filename | ForEach-Object {FMSmartVecToDat.exe -v2d $_.fullname}
+  Get-ChildItem $artifacts | ForEach-Object {
+    $newname = $_.fullname -replace '_v2d.dat', '.dat'
+    if (Test-Path -Path $newname) { Remove-Item -Path $newname }
+    Rename-Item -Path $_.FullName -NewName $newname
+    }
+}
